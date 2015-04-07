7b9c890d0b911cfef62595f4ecda5d901d9e427d
xiq xiao <xiqxiq@rocketmail.com>
2015-04-07 21:06:45 +0200

    hel

--- assets/javascript/scroll.js
+++ assets/javascript/scroll.js
@@ -1,11 +1,13 @@
    $(window).scroll(function() {
 
-    if ($(this).scrollTop()>128)
+    if ($(this).scrollTop()>256)
      {
         $('.hide-on-scroll').fadeOut();
+        $('.show-on-scroll').fadeIn();
      }
     else
      {
       $('.hide-on-scroll').fadeIn();
+      $('.show-on-scroll').fadeOut();
      }
  });
